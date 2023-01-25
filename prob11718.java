191cee561b6009ba0cfac4931b53216aab63afdf
Jooyeon-Kimm <jooooyeonkim@gmail.com>
2023-01-25 16:16:11 +0900

    input equals output
    입력 받은 대로 출력하기

---
+++ prob11718.java
@@ -0,0 +1,27 @@
+package baekjoon;
+
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.PriorityQueue;
+import java.util.Scanner;
+import java.util.StringTokenizer;
+
+public class prob11718 {
+	public static void main(String[] args) throws IOException 
+	{
+		Scanner sc = new Scanner(System.in);
+		 
+		while (sc.hasNext()) {
+		    System.out.println(sc.nextLine());
+		}
+
+
+	}
+	
+	
+
+}
